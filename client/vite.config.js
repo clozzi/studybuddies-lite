72589ff6a66db9bdcb104bfc9c32335d090715e4
Chris Lozzi <clozzi12@gmail.com>
2024-05-11 14:22:59 -0400

    FE deps and config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    cors: true,
+    proxy: {
+      "/api": "http://localhost:5555/"
+    },
+    '/socket.io': {
+      target: 'ws://localhost:5555',
+      ws: true,
+    }
+  },
 })
